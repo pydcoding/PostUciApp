6e59decf2d5fd39c751252f5531169c3efb24b89
Daniel Mancebo <dani.mancebo_3@hotmail.com>
2020-01-07 18:53:25 +0100

    feat: a bunch of strings added

--- app/src/main/java/com/pyd/postuciapp/activity/TestIntroductionActivity.java
+++ app/src/main/java/com/pyd/postuciapp/activity/TestIntroductionActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
 
 import com.pyd.postuciapp.R;
 import com.pyd.postuciapp.bean.Test;
@@ -19,6 +20,12 @@ public class TestIntroductionActivity extends AppCompatActivity {
         setContentView(R.layout.test_heading);
 
         final Test.TestType testType = (Test.TestType) getIntent().getSerializableExtra("type");
+
+        switch (testType) {
+            case ESCALA_IMPACTO_EVENTO:
+                ((AppCompatTextView)findViewById(R.id.test_heading_heading)).setText(getResources().getString(R.string.test_heading_eie));
+                break;
+        }
 
         findViewById(R.id.test_heading_understood_button).setOnClickListener(new View.OnClickListener() {
             @Override
